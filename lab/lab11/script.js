6e2464e859b2b49230b6d3b279eb95b8ef0c2475
Vladkeeks <147250546+Vladkeeks@users.noreply.github.com>
2023-12-11 13:42:36 +0200

    12

--- lab/lab11/script.js
+++ lab/lab11/script.js
@@ -1,15 +1,32 @@
-
-.example {
-    margin-bottom: 20px;
+// jQuery .html() Method Example
+function changeHTML() {
+    $('#htmlContent').html('<p>New content added through .html() method</p>');
   }
   
-  #content {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
+  // jQuery .css() Method Example
+  function applyStyles() {
+    $('#cssContent').css({
+      'color': 'red',
+      'font-size': '20px',
+      'background-color': 'lightblue'
+    });
   }
   
-  button {
-    margin-top: 10px;
+  // jQuery .val() Method Example
+  function changeValue() {
+    $('#inputField').val('New value');
+  }
+  
+  // jQuery .attr() Method Example
+  function changeAttributes() {
+    $('#image').attr({
+      'alt': 'Changed Image Alt Text',
+      'src': 'https://via.placeholder.com/300' // Changing image source for demonstration
+    });
+  }
+  
+  // jQuery .removeAttr() Method Example
+  function enableButton() {
+    $('#disableBtn').removeAttr('disabled');
   }
   
