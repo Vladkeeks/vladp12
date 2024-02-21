0881771cada4db10af776deaa7c99f5c23ffec5a
Vladkeeks <147250546+Vladkeeks@users.noreply.github.com>
2024-02-21 13:10:51 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -17,4 +17,9 @@ echo "Файл був змінений: $LastModified"."<br>";
 
 </body>
 <?php
+
+$LastModified_unix = strtotime(date("D, d M Y H:i:s", filectime($_SERVER['SCRIPT_FILENAME']))); 
+$LastModified = gmdate("D, d M Y H:i:s \G\M\T", $LastModified_unix); 
+echo "Файл був змінений: $LastModified"."<br>";
+
 ?>
