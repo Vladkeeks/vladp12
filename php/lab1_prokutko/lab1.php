569809dba391d19d400202ca5856513740389498
Vladkeeks <147250546+Vladkeeks@users.noreply.github.com>
2024-02-21 13:22:09 +0200

    Update lab1.php

--- php/lab1_prokutko/lab1.php
+++ php/lab1_prokutko/lab1.php
@@ -11,6 +11,7 @@ $ULogin=$_GET["ULogin"];
 <h3>Приклади з теорії</h3>
 <a href="example1_1_5_1.php">Доступ до змінних форми post (приклад 1.1.5.1) </a><br>
 <a href="example1_1_5_2.php">Доступ до змінних форми get (приклад 1.1.5.2)</a><br>
+ <a href="example1.php">Завдання 7</a><br>
 <a href="operator_if.php">if, eseif, else (приклади з 1.1.15,1.1.16,1.1.17, 1.1.18 )</a><br>
 <a href="operator_switch.php">switch (приклад 1.1.19)</a><br>
 
