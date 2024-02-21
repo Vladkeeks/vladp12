1959f0b5b81057a1237a233843ff7430a0e5244b
Vladkeeks <147250546+Vladkeeks@users.noreply.github.com>
2024-02-21 13:17:57 +0200

    Update lab1.php

--- php/lab1_prokutko/lab1.php
+++ php/lab1_prokutko/lab1.php
@@ -5,26 +5,7 @@ $zm=$_GET["zm"];
 if (!empty($_GET["ULogin"])) {echo "<br>Значення переданої змінної ULogin ".$_GET["ULogin"];} else {echo "<br>zminna ULogin not fount";}
 $ULogin=$_GET["ULogin"];
  ?>
-
- <html>
-<head>
-    <title>Lab 1</title>
-</head>
-<body>
-    <h1>Lab 1: PHP Operators</h1>
-    <ul>
-        <li><a href="examples/example_1_1_1.php">Example 1.1.1: PHP Style Descriptors</a></li>
-        <li><a href="examples/example_1_1_2.php">Example 1.1.2: PHP Operators</a></li>
-        <li><a href="examples/example_1_1_3.php">Example 1.1.3: Spaces</a></li>
-        <li><a href="examples/example_1_1_4.php">Example 1.1.4: Comments</a></li>
-        <li><a href="examples/example_1_1_5.php">Example 1.1.5: Accessing Form Variables</a></li>
-        <li><a href="examples/example_1_1_6.php">Example 1.1.6: String Concatenation</a></li>
-        <li><a href="examples/example_1_1_7.php">Example 1.1.7: Identifiers</a></li>
-        <li><a href="examples/example_1_1_8.php">Example 1.1.8: Variable Types</a></li>
-    </ul>
-</body>
-</html>
-
+<html>
 <title>PHP</title> 
 <body>
 <h3>Приклади з теорії</h3>
